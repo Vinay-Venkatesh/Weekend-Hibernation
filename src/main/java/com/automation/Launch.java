6a6256d7638ded5d2134f9e7d6b81f4109bcd747
Vinay <36667412+xxCoDeBoX@users.noreply.github.com>
2020-05-14 00:21:23 +0530

    Update in Method for Lambda Execution

--- src/main/java/com/automation/Launch.java
+++ src/main/java/com/automation/Launch.java
@@ -4,17 +4,16 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
 public class Launch implements RequestHandler<Object, Object> {
-	// public static void main(String[] args) {
 	public Object handleRequest(Object arg0, Context arg1) {
 		try {
 			Autoscaling autoscaling = new Autoscaling();
 			autoscaling.stopInstances();
+			
+			RDS rds = new RDS(); 
+			rds.stopRds();
 		} catch (Exception e) {
 			return e.getMessage();
 		}
 		return "Success";
-		/*
-		 * RDS rds = new RDS(); rds.stopRds();
-		 */
 	}
 }
